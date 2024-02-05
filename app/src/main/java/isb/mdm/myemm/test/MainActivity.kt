79b59cac44ca32a295f35a6edd45115dbaafc57b
tk20211228 <t3kuboki@gmail.com>
2024-02-05 14:03:01 +0900

    initial commit

---
+++ app/src/main/java/isb/mdm/myemm/test/MainActivity.kt
@@ -0,0 +1,11 @@
+package isb.mdm.myemm.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
