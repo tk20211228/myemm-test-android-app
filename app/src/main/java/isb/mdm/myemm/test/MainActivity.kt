4b204c29892b7499dd18f5186a29eec20999aab6
tk20211228 <t3kuboki@gmail.com>
2024-02-13 14:15:40 +0900

    permissionTest

--- app/src/main/java/isb/mdm/myemm/test/MainActivity.kt
+++ app/src/main/java/isb/mdm/myemm/test/MainActivity.kt
@@ -6,7 +6,9 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.provider.Settings
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
@@ -16,37 +18,22 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val CAMERA_REQUEST_CODE = 101
         const val OTHER_PERMISSIONS_REQUEST_CODE = 102
-//        const val ACTIVITY_RECOGNITION_REQUEST_CODE = 103
-//        const val BODY_SENSORS_REQUEST_CODE = 104
-//        const val READ_CALENDAR_REQUEST_CODE = 105
-//        const val WRITE_CALENDAR_REQUEST_CODE = 106
-//        const val READ_CONTACTS_REQUEST_CODE = 107
-//        const val WRITE_CONTACTS_REQUEST_CODE = 108
-//        const val ACCESS_FINE_LOCATION_REQUEST_CODE = 109
-//        const val ACCESS_COARSE_LOCATION_REQUEST_CODE = 110
-//        const val RECORD_AUDIO_REQUEST_CODE = 111
-//        const val READ_PHONE_STATE_REQUEST_CODE = 112
-//        const val CALL_PHONE_REQUEST_CODE = 113
-//        const val READ_CALL_LOG_REQUEST_CODE = 114
-//        const val WRITE_CALL_LOG_REQUEST_CODE = 115
-//        const val SEND_SMS_REQUEST_CODE = 116
-//        const val RECEIVE_SMS_REQUEST_CODE = 117
-//        const val READ_SMS_REQUEST_CODE = 118
-//        const val RECEIVE_WAP_PUSH_REQUEST_CODE = 119
-//        const val RECEIVE_MMS_REQUEST_CODE = 120
-//        const val READ_EXTERNAL_STORAGE_REQUEST_CODE = 121
-//        const val WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 122
-
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupPermissions()
+
+        // ボタンのクリックリスナーを設定
+        val buttonAppInfo = findViewById<Button>(R.id.button_app_info)
+        buttonAppInfo.setOnClickListener {
+            // アプリの設定画面を開く
+            openAppSettings()
+        }
     }
     private fun setupPermissions() {
-        val permissionsRequired = arrayOf(
+        val permissionsRequired = mutableListOf(
             Manifest.permission.CAMERA,
-            Manifest.permission.ACTIVITY_RECOGNITION,
             Manifest.permission.BODY_SENSORS,
 
             Manifest.permission.READ_CALENDAR,
@@ -73,6 +60,11 @@ class MainActivity : AppCompatActivity() {
             Manifest.permission.WRITE_EXTERNAL_STORAGE
         )
 
+        // Android 10 (APIレベル 29) 以降の場合のみ、ACTIVITY_RECOGNITIONパーミッションを追加
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            permissionsRequired.add(Manifest.permission.ACTIVITY_RECOGNITION)
+        }
+
         val permissionsNotGranted = permissionsRequired.filter {
             ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
         }
@@ -85,6 +77,10 @@ class MainActivity : AppCompatActivity() {
                                             permissions: Array<String>,
                                             grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val deniedPermissions = permissions.zip(grantResults.toTypedArray())
+            .filter { it.second != PackageManager.PERMISSION_GRANTED }
+            .map { it.first }
+
         when (requestCode) {
             CAMERA_REQUEST_CODE -> {
                 if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
@@ -92,44 +88,56 @@ class MainActivity : AppCompatActivity() {
                     AlertDialog.Builder(this)
                         .setTitle("パーミッション承認")
                         .setMessage("カメラパーミッションが承認されました！")
+                        .show()
+                } else {
+                    // ユーザーがパーミッションを拒否した時の処理
+                    AlertDialog.Builder(this)
+                        .setTitle("パーミッション承認")
+                        .setMessage("カメラパーミッションが拒否されました")
                         .setPositiveButton("OK") { dialog, which ->
                             // アプリの設定画面を開く
                             val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
                             val uri = Uri.fromParts("package", packageName, null)
                             intent.data = uri
                             startActivity(intent)
-
                         }
                         .show()
-                } else {
-                    // ユーザーがパーミッションを拒否した時の処理
                 }
             }
             OTHER_PERMISSIONS_REQUEST_CODE -> {
                 // 他のパーミッションの結果の処理
                 if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
                     // すべてのパーミッションが承認された場合の処理
-
-                    // アプリの設定画面を開く
-                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                    val uri = Uri.fromParts("package", packageName, null)
-                    intent.data = uri
-                    startActivity(intent)
+                    // パーミッションが承認された時の処理
+                    AlertDialog.Builder(this)
+                        .setTitle("パーミッション承認")
+                        .setMessage("全てのパーミッションが承認されました！")
+                        .show()
                 } else {
                     // 一部またはすべてのパーミッションが拒否された場合の処理
-                    // アプリの設定画面を開く
-                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                    val uri = Uri.fromParts("package", packageName, null)
-                    intent.data = uri
-                    startActivity(intent)
+                    // パーミッションが承認された時の処理
+                    // 拒否されたパーミッションをリスト表示する
+                    val deniedPermissionsList = deniedPermissions.joinToString("\n")
+                    AlertDialog.Builder(this)
+                        .setTitle("パーミッション承認")
+                        .setMessage("以下のパーミッションが拒否されました:\n$deniedPermissionsList")
+                        .setPositiveButton("OK") { dialog, which ->
+                            // アプリの設定画面を開く
+                            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                            val uri = Uri.fromParts("package", packageName, null)
+                            intent.data = uri
+                            startActivity(intent)
+                        }
+                        .show()
                 }
             }
-
-
-
-
         }
     }
-
+    private fun openAppSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivity(intent)
+    }
 
 }
